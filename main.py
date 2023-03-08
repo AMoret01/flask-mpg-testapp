c9f5435add67f8d3f0f72e92919e2bf94912192e
AMoret01 <57366094+AMoret01@users.noreply.github.com>
2023-03-08 20:19:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask,jsonify,request
+import pickle
 
-app = Flask(__name__)
+
+app=Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/", methods=["GET"])
+def home():
+    return """
+    <h1> App para calcular MPG</h1>
+    App para testear flask y Railway
+    """
+
+@app.route("/api/v1/predictions", methods=["GET"])
+def predictions():
+    cylinders=request.args["cylinders"]
+    displacement=request.args["displacement"]
+    horsepower=request.args["horsepower"]
+    acceleration=request.args["acceleration"]
+    weight=request.args["weight"]
+    model_year=request.args["model_year"]
+
+    map_origin={'usa': 1, 'japan': 2, 'europe': 3}
+    origin=request.args["origin"]
+    origin=map_origin[origin]
+
+    filename="model.save"
+    loaded_model= pickle.load(open(filename,"rb"))
+    
+    # El orden de los datos tiene que ser el mismo que en el modelo
+    new_data=[cylinders,displacement,horsepower,weight,acceleration,model_year,origin]
+
+    return jsonify(loaded_model.predict([new_data])[0])
 
 
 if __name__ == '__main__':
